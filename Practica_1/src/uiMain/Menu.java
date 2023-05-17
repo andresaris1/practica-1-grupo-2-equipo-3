06483294de13acdfa6eb6809fa7971b4ef26459c
MenGonz <semendozag@unal.edu.co>
2023-05-17 15:43:30 -0500

    Update Menu.java

--- Practica_1/src/uiMain/Menu.java
+++ Practica_1/src/uiMain/Menu.java
@@ -1,6 +1,8 @@
 package uiMain;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import gestorAplicacion.Usuario;
 import uiMain.Main;
@@ -12,13 +14,12 @@ public class Menu {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("- - - FUNCIONALIDADES - - - -");
-        System.out.println("1.Reserva");
-        System.out.println("2.Pagos");
-        System.out.println("3.Tours");
-        System.out.println("4.Adicion servicios");
-        System.out.println("5.Check In");
-        System.out.println("6.Check out");
+        System.out.println("-- - - FUNCIONALIDADES - - - -");
+        System.out.println("1.Reservar Alojamiento");
+        System.out.println("2.Reservar Turística");
+        System.out.println("3.Reservar Evento");
+        System.out.println("4.Cobro");
+        System.out.println("5.Mostrar información de habitaciones");
 
         System.out.println("Eliga una opción: ");
         int opcion = sc.nextInt();
@@ -29,29 +30,42 @@ public class Menu {
                 break;
             case 2:
                 System.out.println("Ingrese la identificacion del usuario: ");
-                int id = sc.nextInt();
-
-                for (int i = 0; i < Main.ListaUsuarios.length; i++) {
-                    int x=Main.ListaUsuarios[i].getIdentificacion();
-                    if (id==x){
-                        System.out.println("Es la misma, pagaaaaa");
-                    }2
-                    
-
-                }
+                String id = sc.next();
 
                 break;
             case 3:
                 // Codigo de la funcionalidad reserva de tours
                 break;
             case 4:
-                // Codigo para la funcionalidad de agregar servicios
+                // Codigo para la funcionalidad Reserva de eventos
+                System.out.println("Escribe tu identificacion: ");
+                id = sc.next();
+                System.out.println("Escribe el lugar en el que desea su evento: ");
+                System.out.println("1. Terraza");
+                System.out.println("2. Piscina");
+                System.out.println("3. Salon de eventos");
+                System.out.println("4. Gran salón de eventos");
+                int tipoLugar = sc.nextInt();
+
+                
+                HashMap<String, Integer> empleadosNecesarios = new HashMap<String, Integer>();
+                
+                System.out.println("¿Cuántos cocineros requiere?");
+                empleadosNecesarios.put("Chefs", sc.nextInt());
+
+                System.out.println("¿Cuántos meseros requiere?");
+                empleadosNecesarios.put("Meseros", sc.nextInt());
+                
+                System.out.println("¿Cuántos bartenders requiere?");
+                empleadosNecesarios.put("Bartender", sc.nextInt());
+
+                
                 break;
             case 5:
-                // codigo para check in
+                // codigo para la funcionalidad de cobro
                 break;
             case 6:
-                // Codigo para check out
+                // Codigo para la funcionalidad de mostrar informacion de habitaciones
                 break;
 
             default:
