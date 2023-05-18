5da2ee3135c8f3fec12b5c687afb695a79bea44f
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-05-18 13:36:42 -0500

    Update Destinos.java

--- Practica_1/src/gestorAplicacion/Destinos.java
+++ Practica_1/src/gestorAplicacion/Destinos.java
@@ -1,10 +1,19 @@
 package gestorAplicacion;
 
 public enum Destinos {
-    TORRE_EIFFEL = 60
-    MUSEO_LOUVRE = 80
-    ARCO_TRIUNFO = 50
-    NOTRE_DAME = 70
-    PALACIO_VERSALLES = 90
+    TORRE_EIFFEL(60),
+    MUSEO_LOUVRE(80),
+    ARCO_TRIUNFO(50),
+    NOTRE_DAME(70),
+    PALACIO_VERSALLES(90);
 
+    private int valor;
+
+    private Destinos(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
 }
