eb0e402dc726938eb93b75bcb419298b2d1ff3ce
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-05-18 19:32:41 -0500

    Update Destinos.java

--- Practica_1/src/gestorAplicacion/Destinos.java
+++ Practica_1/src/gestorAplicacion/Destinos.java
@@ -6,6 +6,10 @@ public enum Destinos {
     ARCO_TRIUNFO(50),
     NOTRE_DAME(70),
     PALACIO_VERSALLES(90);
+    COMBO_SIMPLE_TRAE_DESTINOS_1Y3(100),
+    COMBO_NORMAL_TRAE_DESTINOS_2Y4(140),
+    COMBO_COMPLETO_TRAE_TODOS(320),
+    
 
     private int valor;
 
