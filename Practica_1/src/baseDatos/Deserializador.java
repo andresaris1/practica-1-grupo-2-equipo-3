bab0224e5b741ed7742e993ffa10f19c468ef4b6
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-05-21 14:46:35 -0500

    Create Deserializador.java

---
+++ Practica_1/src/baseDatos/Deserializador.java
@@ -0,0 +1,64 @@
+package baseDatos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+import uiMain.*;
+import gestorAplicacion.*;
+
+public class Deserializador {
+	private static File ruta = new File("src\\baseDatos\\temp");
+
+	public static void deserializar(Main main) {
+		File[] docs = ruta.listFiles();
+		FileInputStream fis;
+		ObjectInputStream ois;
+
+		for (File file : docs) {
+			if (file.getAbsolutePath().contains("clientes")) {
+				try {
+					fis = new FileInputStream(file);
+					ois = new ObjectInputStream(fis);
+
+					main.setClientes((List<Usuario>) ois.readObject());
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			} else if (file.getAbsolutePath().contains("facturas")) {
+				try {
+					fis = new FileInputStream(file);
+					ois = new ObjectInputStream(fis);
+
+					main.setFacturas((List<Factura>) ois.readObject());
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			} else if (file.getAbsolutePath().contains("reservas")) {
+				try {
+					fis = new FileInputStream(file);
+					ois = new ObjectInputStream(fis);
+
+					main.setReservas((List<Reserva>) ois.readObject());
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+}
