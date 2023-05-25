4367edff7a579124b1ddb7ca618727752ef8af69
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-05-24 19:52:28 -0500

    ...

--- Practica_1/src/uiMain/objetoss.java
+++ Practica_1/src/uiMain/objetoss.java
@@ -1,9 +1,25 @@
 package uiMain;
+import java.util.ArrayList;
+
+import gestorAplicacion.*;
 
 import uiMain.*;
 //CLase temporar para la creacion de los objetos estables de la serializacion
 
 public class objetoss {
+
+    Usuario usuario1 = new Usuario("carlos", 1234, 0, null, "", new ArrayList<Factura>());
+	Usuario usuario2 = new Usuario("Maria", 345, 0, null, "",new ArrayList<Factura>() );
+	Usuario usuario3 = new Usuario("Ximena", 763, 0, null, "", new ArrayList<Factura>());
+
+    Empleado empleado1 = new Empleado(null, 0, null);
+
+    Lugar lugar1= new Lugar("Terraza");
+    Lugar lugar2= new Lugar("Piscina");
+    Lugar lugar3= new Lugar("Salon");
+
+
+
 
     
 }
