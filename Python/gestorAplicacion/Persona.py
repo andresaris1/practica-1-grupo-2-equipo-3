95b59e4757675d51d044208bc8d502195aefec78
andresaris1 <124937113+andresaris1@users.noreply.github.com>
2023-06-04 15:07:57 -0500

    Update Persona.py

--- Python/gestorAplicacion/Persona.py
+++ Python/gestorAplicacion/Persona.py
@@ -1,5 +1,29 @@
+import abc
 
+class Persona(abc.ABC):
+    def __init__(self, nombre, identificacion, telefono):
+        self.nombre = nombre
+        self.identificacion = identificacion
+        self.telefono = telefono
 
+    @abc.abstractmethod
+    def informacion(self):
+        pass
 
-class Persona:
-    pass
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def get_identificacion(self):
+        return self.identificacion
+
+    def set_identificacion(self, identificacion):
+        self.identificacion = identificacion
+
+    def get_telefono(self):
+        return self.telefono
+
+    def set_telefono(self, telefono):
+        self.telefono = telefono
