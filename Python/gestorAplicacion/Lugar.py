67b79269edb9e1a8be147dd458dd6ff8c53ce20e
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-06-06 17:48:10 -0500

    ...

--- Python/gestorAplicacion/Lugar.py
+++ Python/gestorAplicacion/Lugar.py
@@ -1,4 +1,30 @@
+from gestorAplicacion.Servicio import Servicio
 
 
-class Lugar:
-    pass
+class Lugar(Servicio):
+    
+    def __init__(self,nombre, valor,descripcion, numero,capacidad):
+        super().__init__(nombre, valor, descripcion)
+        self._numero=0
+        self._capacidad=capacidad
+        #El valor no se ingresa porque es calculado mediante la funcion ValorSegunTipo y el numero de los lugares de eventos es 0 por defecto
+
+
+        super().setValor(self.valorSegunTipo(nombre))
+
+    def valorSegunTipo(nombre):
+        if nombre == "Habitación familiar":
+            return 100000
+        elif nombre == "Habitación doble":
+            return 80000
+        elif nombre == "Habitación individual":
+            return 50000
+        elif nombre == "Terraza":
+            return 200000
+        elif nombre == "Piscina":
+            return 300000
+        elif nombre == "Salon":
+            return 500000
+        else:
+            return -1
+
