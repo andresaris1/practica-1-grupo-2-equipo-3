35e697aa6a3ad380b60059d36494972575deb45e
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-06-08 10:16:05 -0500

    ...

--- Python/uiMain/Menu.py
+++ Python/uiMain/Menu.py
@@ -1,17 +1,17 @@
-if __name__ == '__Menu__':
-    print("dfg")
-    from Python.gestorAplicacion.modelos.Empleado import Empleado
-    from Python.gestorAplicacion.modelos.Usuario import Usuario
-    from Python.gestorAplicacion.reservacion.Factura import Factura
-    from Python.gestorAplicacion.reservacion.Servicio import Servicio
 
-    print("Pruebas")
+from Python.gestorAplicacion.modelos.Empleado import Empleado
+from Python.gestorAplicacion.modelos.Usuario import Usuario
+from Python.gestorAplicacion.reservacion.Factura import Factura
+from Python.gestorAplicacion.reservacion.Servicio import Servicio
+
+print("Pruebas")
 
 
-    print("Prueba de factura")
-    ser1 = Servicio( "Cocina", 200,"dsrfvdfvg")
-    emo1 = Empleado("Carla", 123456789, 135, "Bar")
-    use1 = Usuario( "Kevin", 6543, 321, "tipo", 678)
-    fac1 = Factura(use1, emo1, [ser1], [], "concepto")
+print("Prueba de factura")
+ser1 = Servicio( "Cocina", 200,"dsrfvdfvg")
+emo1 = Empleado("Carla", 123456789, 135, "Bar")
+use1 = Usuario( "Kevin", 6543, 321, "tipo", 678)
+fac1 = Factura(use1, emo1, [ser1], [], "concepto")
 
-    fac1.imprimirFactura()
+fac1.imprimirFactura()
+print("dfg")
