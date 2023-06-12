9e625f171bec339086cebb684773197d06b3b8f9
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-06-12 16:50:54 -0500

    ...

--- Python/uiMain/Menu.py
+++ Python/uiMain/Menu.py
@@ -9,7 +9,11 @@ menuBar=Menu(ventanaInicio)
 ventanaInicio.config(menu=menuBar)
 
 menuInicio=Menu(menuBar)
-menuBar.add_cascade(label="Menu 123", menu=menuInicio)
+menuBar.add_cascade(label="Inicio", menu=menuInicio)
+menuInicio.add_command(label="Salir",)
+menuInicio.add_command(label="Descripción",)
+
+menuInicio.config(activebackground="sienna1")
 
 ventanaInicio.rowconfigure(0, weight=1)  
 ventanaInicio.columnconfigure(0, weight=1) 
@@ -24,8 +28,17 @@ frame2 = Frame(ventanaInicio, bg="DeepSkyBlue2",borderwidth=1, relief="solid")
 frame2.grid(row=0, column=1, sticky="nsew", padx=3) 
 
 
-frameP3=Frame(frame1, bg="red", height=200, width=200)
-frameP3.place(relheight=0.3, relwidth=1)
+frameP3=Frame(frame1, bg="purple", height=200, width=200, borderwidth=1, relief="solid")
+frameP3.place(relheight=0.35 ,relwidth=1 , relx=0 ,rely=0)
+
+frameP4=Frame(frame1, bg="green", height=200, width=200, borderwidth=1, relief="solid")
+frameP4.place(relheight=0.65 ,relwidth=1 , relx=0 ,rely=0.35)
+
+frameP5=Frame(frame2, bg="red", height=200, width=200, padx=3, pady=3)
+frameP5.place(relheight=0.35 ,relwidth=1 , relx=0 ,rely=0)
+
+frameP6=Frame(frame2, bg="orange", height=200, width=200, padx=3, pady=3)
+frameP6.place(relheight=0.65 ,relwidth=1 , relx=0 ,rely=0.35)
 
 
 ventanaInicio.mainloop()
