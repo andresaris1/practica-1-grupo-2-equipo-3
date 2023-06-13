ca2281bf520ada64eac5786f8b8d8e9d833fa896
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-06-13 13:47:48 -0500

    ...

--- Python/uiMain/Pruebas.py
+++ Python/uiMain/Pruebas.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     ser2 = Servicio( "Limpieza", 10,"dsrfvdfvg")
     ser3 = Servicio( "Piscina", 10,"dsrfvdfvg")
     emo1 = Empleado("Carla", 123456789, 135, "Bar")
-    use1 = Usuario( "Kevin", 6543, 321, "tipo", 678)
+    use1 = Usuario( "luis", 6543, 321, "tipo", 678)
     fac1 = Factura(use1, emo1, [ser1,ser2,ser3], "concepto")
     fac2 = Factura(use1, emo1, [ser1,ser2,ser3], "concepto")
     fac3 = Factura(use1, emo1, [ser1,ser2,ser3], "concepto")
@@ -27,6 +27,6 @@ if __name__ == '__main__':
     for usuario in Almacenamiento.listaUsuarios:
         print(usuario.nombre)
 
-    #Almacenamiento.listaUsuarios.append(use1)
+    Almacenamiento.listaUsuarios.append(use1)
 
     Almacenamiento.Serializar()
