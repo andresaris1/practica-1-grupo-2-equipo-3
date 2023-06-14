5aad76801487e84aa98ca66eeb7bbff79f5688fe
CarolinaHumanez <124937617+CarolinaHumanez@users.noreply.github.com>
2023-06-13 23:14:18 -0500

    ...

--- Python/uiMain/Menu.py
+++ Python/uiMain/Menu.py
@@ -4,10 +4,22 @@ from PIL import Image, ImageTk
 
 import os
 
+
 def AbrirFuncional():
     ventanaFuncional.state(newstate = "normal")
     ventanaInicio.state(newstate = "withdraw")
-    
+
+def Carrusel(event):
+    global indiceImagen
+    global listImage
+
+    if indiceImagen < 4:
+        indiceImagen += 1
+    else:
+        indiceImagen = 1
+
+    nextImagen=listImage[indiceImagen]
+    botonCarrusel.config(image=nextImagen)
 
 #CREACION DE LA VENTANA DE INICIO
 ventanaInicio = Tk()
@@ -24,29 +36,29 @@ menuInicio.add_command(label="Descripción",)
 
 menuInicio.config(activebackground="sienna1")
 
-ventanaInicio.rowconfigure(0, weight=1)  
-ventanaInicio.columnconfigure(0, weight=1) 
+ventanaInicio.rowconfigure(0, weight=1)
+ventanaInicio.columnconfigure(0, weight=1)
 
 ventanaInicio.columnconfigure(0, weight=1)
 ventanaInicio.columnconfigure(1, weight=1)
 
-frame1 = Frame(ventanaInicio, bg="lightpink1", borderwidth=1, relief="solid")
+frame1 = Frame(ventanaInicio, bg="light steel blue", borderwidth=1, relief="solid")
 frame1.grid(row=0, column=0, sticky="nsew", padx=3)
 
-frame2 = Frame(ventanaInicio, bg="DeepSkyBlue2",borderwidth=1, relief="solid")
-frame2.grid(row=0, column=1, sticky="nsew", padx=3) 
+frame2 = Frame(ventanaInicio, bg="light steel blue",borderwidth=1, relief="solid")
+frame2.grid(row=0, column=1, sticky="nsew", padx=3)
 
 
-frameP3=Frame(frame1, bg="purple", height=200, width=200, borderwidth=1, relief="solid")
+frameP3=Frame(frame1, bg="white", height=200, width=200, borderwidth=1, relief="solid")
 frameP3.pack(side="top", fill="x", padx=3, pady=3)
 
-frameP4=Frame(frame1, bg="green", height=200, width=200, borderwidth=1, relief="solid")
+frameP4=Frame(frame1, bg="white", height=200, width=200, borderwidth=1, relief="solid")
 frameP4.pack(expand=True, fill="both", padx=3, pady=3)
 
-frameP5=Frame(frame2, bg="red", height=200, width=200, borderwidth=1, relief="solid")
+frameP5=Frame(frame2, bg="white", height=200, width=200, borderwidth=1, relief="solid")
 frameP5.pack(side="top", fill="x", padx=3, pady=3)
 
-frameP6=Frame(frame2, bg="orange", height=200, width=200, borderwidth=1, relief="solid")
+frameP6=Frame(frame2, bg="white", height=200, width=200, borderwidth=1, relief="solid")
 frameP6.pack(expand=True, fill="both", padx=3, pady=3)
 
 #Zona P3 Bienvenidad
@@ -54,14 +66,25 @@ Bienvenida=Label(frameP3,text="¡Bienevenido! \n Este es el nuevo sistema\n de g
 Bienvenida.place(relx=0.5, rely=0.5, relheight=0.5, relwidth=0.7, anchor='center')
 
 #Zona P4 Carrusel de imagnes y Boton de incio
-path=os.path.join(os.path.dirname(__file__), "img/hotel.png")
+pathImgHotel=os.path.join(os.path.dirname(__file__), "img/hotel.png")
+pathImgPiscina=os.path.join(os.path.dirname(__file__), "img/piscina.png")
+pathImgDestino1=os.path.join(os.path.dirname(__file__), "img/destino1.png")
+pathImgDestino2=os.path.join(os.path.dirname(__file__), "img/destino2.png")
 
-imgHotel=tk.PhotoImage(file=path)
-labelCarrusel=Label(frameP4,image=imgHotel)
-labelCarrusel.pack(side="top")
+pathImgDef=os.path.join(os.path.dirname(__file__), "img/def.png")
 
-BotonInicio=Button(frameP4,text="INCIO", command=AbrirFuncional)
-BotonInicio.pack(side="bottom")
+imgHotel=tk.PhotoImage(file=pathImgHotel)
+imgPiscina=tk.PhotoImage(file=pathImgPiscina)
+imgDestino1=tk.PhotoImage(file=pathImgDestino1)
+imgDestino2=tk.PhotoImage(file=pathImgDestino2)
+imgDef=tk.PhotoImage(file=pathImgDef)
+
+listImage=[imgDef, imgHotel, imgPiscina, imgDestino1, imgDestino2]
+indiceImagen=0
+
+botonCarrusel=Button(frameP4,image=imgHotel)
+botonCarrusel.pack(expand=True)
+botonCarrusel.bind("<Enter>",Carrusel)
 
 
 #CREACION DE LA VENTANA FUNCIONAL
